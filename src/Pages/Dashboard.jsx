24157816c004be29d6ad9a856d4c257f5f9bd00d
umarmohammad96 <onesownumar@gmail.com>
2024-08-31 23:12:44 +0530

    modified dashboard.jsx

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -12,11 +12,7 @@ const Dashboard = ({ selected }) => {
           Smart Home Utility Management is designed to revolutionize the way you
           interact with your home. Our system seamlessly integrates with your
           existing infrastructure to provide real-time monitoring and control of
-          all utilities, from energy and water usage to heating and cooling. By
-          optimizing efficiency and reducing waste, you not only save on costs
-          but also contribute to a more sustainable future. Experience the
-          convenience and peace of mind that comes with managing your home
-          intelligently.
+          all utilities, from energy and water usage to heating and cooling.
         </h3>
       </div>
       <div className="form">
