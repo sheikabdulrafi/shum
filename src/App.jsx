e97bcdd6f14f67cc5febd99ec2cd8c5549cb92fd
umarmohammad96 <onesownumar@gmail.com>
2024-08-31 10:42:40 +0530

    added authenticaiton form

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import "./Styles/styles.scss"
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import Dashboard from "./Pages/Dashboard"
+import "./Styles/styles.scss";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./Pages/Dashboard";
+import SideBar from "./Components/SideBar";
+
 const App = () => {
   return (
     <Router>
+      <SideBar />
       <Routes>
         <Route path="/" element={<Dashboard />} />
       </Routes>
     </Router>
-  )
-}
+  );
+};
 export default App;
--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,15 +1,23 @@
 import React from "react";
-import SideBar from "../Components/SideBar";
-import Home from "../Components/Home";
+import AuthPage from "../Components/Auth";
+import bgimage from "../assets/bgimage.png";
 
 const Dashboard = ({ selected }) => {
   return (
-    <>
-      <SideBar />
-      <section>
-        {selected === "home" ? <Home /> : <h1>Nothing Selected</h1>}
-      </section>
-    </>
+    <section className="Authentication">
+      <img src={bgimage} className="bgImage" />
+      <div>
+        <h1 className="titleData">Smart Home Utility Management</h1>
+        <h3>
+          Konthamandi panikimalina vallu chesey paniki raani project. Pani
+          chestado ledo kooda teliyadu. if else lo rasey code ki AI model
+          chesaru... adikooda nibba lagaey output isthundi... #AreyEntraIdhi
+        </h3>
+      </div>
+      <div className="form">
+        <AuthPage />
+      </div>
+    </section>
   );
 };
 
